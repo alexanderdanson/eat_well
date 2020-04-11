f0213a7ef886ffe90d7e7f58c3957846fd9a78a0
Alexander Danson <alex.danson2@gmail.com>
2020-04-11 11:12:33 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/eat_well/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eat_well
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
